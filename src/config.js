de38648ae3308afaa9027a71b8dcddca0ab5b8f6
Wendy Bartos <54962661+WEB818@users.noreply.github.com>
2020-03-14 13:16:18 -0700

    update api endpoint

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export default {
   API_ENDPOINT:
-    process.env.REACT_APP_API_ENDPOINT || "http://localhost:8080/api",
+    "https://vast-bayou-48826.herokuapp.com/api" || "http://localhost:8080/api",
   TOKEN_KEY: "client-auth-token"
 };
